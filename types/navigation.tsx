18b1ff5b2aba4ab5b9feb34704892db5bd597539
jessejuwe <jessejuwe@gmail.com>
2023-10-07 23:23:07 +0100

    initial commit (new type config)

---
+++ types/navigation.tsx
@@ -0,0 +1,14 @@
+export type RootStackParamList = {
+  Manage: { id: string } | undefined;
+  Tabs: undefined;
+};
+
+export type AuthStackParamList = {
+  Login: undefined;
+  Signup: undefined;
+};
+
+export type BottomTabParamList = {
+  All: undefined;
+  Recent: undefined;
+};
